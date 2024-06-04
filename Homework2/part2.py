81f5dc196f759f964077cccd86a9a68cde329788
Roncooo <francescooroncolatoo@gmail.com>
2024-06-04 11:04:23 +0200

    Added min graph and saving of graphs

--- Homework2/part2.py
+++ Homework2/part2.py
@@ -22,11 +22,11 @@ def callback(future: Future) -> None:
     list = parse_psing_result(result_string)
     future.result = Result(L_byte, list)
 
-def perform_pings_and_save_into_file(K: int, L_byte_step: int, output_file: str, target_name: str, max_threads: int=10):
+def perform_pings_and_save_into_file(K: int, L_byte_step: int, output_file: str, target_name: str, max_threads: int=10, min_L_byte: int=10, max_L_byte: int=1472,):
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
         futures = []
         # Submit tasks to the executor
-        for L_byte in range(min_L_byte,max_L_byte,L_byte_step):
+        for L_byte in range(min_L_byte,max_L_byte+1,L_byte_step):
             temp_file_name = f"ping_result_L_{L_byte}.txt"
             future = executor.submit(psping, target_name=target_name, K=K, result_file=temp_file_name)
             
@@ -52,22 +52,37 @@ def parse_ping_data(file_name: str) -> List[Result]:
 
 
 
-K = 30 # number of packets sent for each L_byte value
+K = 100 # number of packets sent for each L_byte value
 target_name = "atl.speedtest.clouvider.net"
-output_file = "lots_of_pings.txt"
-# perform_pings_and_save_into_file(K=K, L_byte_step=4, output_file=output_file, target_name=target_name)
+L_byte_step=50
+max_threads=10
+output_file = f"lots_of_pings_K{K}_step{L_byte_step}_th{max_threads}.txt"
+perform_pings_and_save_into_file(K=K, L_byte_step=L_byte_step, output_file=output_file, target_name=target_name)
 results: List[Result] = parse_ping_data(output_file)
 
 x = [result.L_bytes for result in results]
 y = [result.rtt_list for result in results]
+plt.figure(1)
 # Create the plot
 for i in range(len(x)):
     plt.scatter([x[i]] * len(y[i]), y[i], s=20, alpha=0.6, edgecolors='w', linewidth=0.5, label=f'Group {x[i]}')
 
 # Add titles and labels
-plt.title('Sample Plot')
-plt.xlabel('X-axis Label')
-plt.ylabel('Y-axis Label')
+plt.title(f'Results of {K} pings to {target_name} with L step of {L_byte_step} bytes')
+plt.xlabel('L (pkt size) - bytes')
+plt.ylabel('RTT - ms')
+plt.savefig(f"lots_of_pings_K{K}_step{L_byte_step}_th{max_threads}_all.png")
 
-# Display the plot
-plt.show()
+
+
+plt.figure(2)
+min_y = [min(result.rtt_list) for result in results]
+# Create the plot
+plt.scatter(x, min_y, s=20, alpha=0.6, edgecolors='w', linewidth=0.5, label=f'Group {x[i]}')
+
+# Add titles and labels
+plt.title(f'Min RTT value')
+plt.xlabel('L (pkt size) - bytes')
+plt.ylabel('RTT_min - ms')
+
+plt.savefig(f"lots_of_pings_K{K}_step{L_byte_step}_th{max_threads}_min.png")
